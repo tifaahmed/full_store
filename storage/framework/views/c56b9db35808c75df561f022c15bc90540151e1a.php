ba652c21fa0110ccd6f77b26b9c434ab5943d297
tifaahmed <tifa.ahmed23@gmail.com>
2023-09-30 19:35:48 +0200

    comman permisions

--- storage/framework/views/c56b9db35808c75df561f022c15bc90540151e1a.php
+++
@@ -1,9 +0,0 @@
-
-<section>
-    <div class="container">
-        <div class="blank-page-height">
-            <img src="<?php echo e(url(env('ASSETSPATHURL').'web-assets/iamges/svg/No_data.svg')); ?>" alt="">
-        </div>
-    </div>
-</section>
-<?php /**PATH /home/mostafa/small_store/new_full_store/resources/views/front/nodata.blade.php ENDPATH**/ ?>
--- storage/framework/views/dae4687ef477007a2e548248820fd633a56d17b2.php
+++
@@ -1,4 +0,0 @@
-
-<div class="preloader" id="preloader">
-    <div class="loader"></div>
-</div><?php /**PATH /var/www/html/full_store/resources/views/front/theme/preloader.blade.php ENDPATH**/ ?>
